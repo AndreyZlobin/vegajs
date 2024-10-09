252651e14a5f69476de84b2ddeac2db4e48fcb5e
AndreyZlobin <zlobin.andy@gmail.com>
2024-10-09 18:18:41 +0300

    chore: update bundle

--- packages/modal-controller/modern.config.ts
+++ packages/modal-controller/modern.config.ts
@@ -3,11 +3,20 @@ import { defineConfig, moduleTools } from '@modern-js/module-tools';
 export default defineConfig({
   plugins: [moduleTools()],
   buildConfig: {
+    target: 'esnext',
     minify: 'terser',
+    jsx: 'automatic',
     buildType: 'bundleless',
+    input: [
+      'src/**/*.ts',
+      'src/**/*.tsx',
+      '!src/**/*.spec.ts',
+      '!src/**/*.spec.tsx',
+      '!src/**/*.test.ts',
+      '!src/**/*.test.tsx',
+    ],
     platform: 'browser',
     tsconfig: './tsconfig.build.json',
-    jsx: 'automatic',
   },
   buildPreset: 'npm-library',
 });
