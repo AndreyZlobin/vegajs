33486af2b26c8ecdb1f4a4ccde89d96ea412f2bd
AndreyZlobin <zlobin.andy@gmail.com>
2024-10-10 06:34:55 +0300

    chore: update

--- release.config.js
+++ release.config.js
@@ -50,21 +50,22 @@ const createSemanticConfig = () => {
         },
       },
     ],
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
-    ],
     ['@semantic-release/github'],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json', 'CHANGELOG.md'],
-        message:
-          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
+
+    // [
+    //   '@semantic-release/changelog',
+    //   {
+    //     changelogFile: 'CHANGELOG.md',
+    //   },
+    // ],
+    // [
+    //   '@semantic-release/git',
+    //   {
+    //     assets: ['package.json', 'CHANGELOG.md'],
+    //     message:
+    //       'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    //   },
+    // ],
   ];
 
   return {
