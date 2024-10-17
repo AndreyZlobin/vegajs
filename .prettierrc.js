aebd8a5d8e07c93a136dc02dcfe4ef07e98cac56
AndreyZlobin <zlobin.andy@gmail.com>
2024-10-17 12:04:29 +0300

    chore: update biome

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,7 @@
-module.exports = require('@self-kit/prettier-config');
+const config = require('@self-kit/prettier-config');
+/**
+ * @type {import("prettier").Options}
+ */
+module.exports = {
+  ...config,
+};
